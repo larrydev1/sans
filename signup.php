f2005d86e4a6b3475a4e0eb3862e9ddb0eb641fd
larrydev1 <59754266+larrydev1@users.noreply.github.com>
2020-01-10 20:43:21 -0800

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,86 @@
+<?php
+  // To make sure we don't need to create the header section of the website on multiple pages, we instead create the header HTML markup in a separate file which we then attach to the top of every HTML page on our website. In this way if we need to make a small change to our header we just need to do it in one place. This is a VERY cool feature in PHP!
+  require "header.php";
+ // added require 01/10/20
+  // include "signup.inc.php"; 
+ 
+?>
+
+    <main>
+      <div class="wrapper-main">
+        <section class="section-default">
+          <h1>Signup</h1>
+          <?php
+          // Here we create an error message if the user made an error trying to sign up.
+          if (isset($_GET["error"])) {
+            if ($_GET["error"] == "emptyfields") {
+              echo '<p class="signuperror">Fill in all fields!</p>';
+            }
+            else if ($_GET["error"] == "invaliduidmail") {
+              echo '<p class="signuperror">Invalid username and e-mail!</p>';
+            }
+            else if ($_GET["error"] == "invaliduid") {
+              echo '<p class="signuperror">Invalid username!</p>';
+            }
+            else if ($_GET["error"] == "invalidmail") {
+              echo '<p class="signuperror">Invalid e-mail!</p>';
+            }
+            else if ($_GET["error"] == "passwordcheck") {
+              echo '<p class="signuperror">Your passwords do not match!</p>';
+            }
+            else if ($_GET["error"] == "usertaken") {
+              echo '<p class="signuperror">Username is already taken!</p>';
+            }
+          }
+          // Here we create a USER CREATED SUCCESS MESSAGE if the new user was created.
+          else if (isset($_GET["signup"])) {
+            if ($_GET["signup"] == "success") {
+              echo '<p class="signupsuccess">Signup successful!</p>';
+              // Added by me 01/09/20 one line below
+              echo "<p>User " . $uidUsers . " created.</p>";
+            }
+          }
+          ?>
+          <!-- signup-submit is the name of the button below apparently not working 01/10/20 added form=" " this date to button -->
+          <form class="form-signup" id="formsignup" action="includes/signup.inc.php" method="post">
+            <?php
+            // Here we check if the user already tried submitting data.
+
+            // We check username.
+            if (!empty($_GET["uid"])) {
+              echo '<input type="text" name="uid" placeholder="Username" value="'.$_GET["uid"].'">';
+            }
+            else {
+              echo '<input type="text" name="uid" placeholder="Username NW">';
+            }
+
+            // We check e-mail.
+            if (!empty($_GET["mail"])) {
+              echo '<input type="text" name="mail" placeholder="E-mail" value="'.$_GET["mail"].'">';
+            }
+            else {
+              echo '<input type="text" name="mail" placeholder="E-mail NW">';
+            }
+            ?>
+            <input type="password" name="pwd" placeholder="Password NW">
+            <input type="password" name="pwd-repeat" placeholder="Repeat password NW">
+            <button type="submit" name="signup-submit" form="formsignup" >Signup</button>
+          </form>
+          <!--
+          NOTES FOR ME BEFORE DOING PHP!
+          <form class="form-signup" action="includes/signup.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username">
+            <input type="text" name="mail" placeholder="E-mail">
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="password" name="pwd-repeat" placeholder="Repeat password">
+            <button type="submit" name="signup-submit">Signup</button>
+          </form>
+          -->
+        </section>
+      </div>
+    </main>
+
+<?php
+  // And just like we include the header from a separate file, we do the same with the footer.
+  require "footer.php";
+?>
